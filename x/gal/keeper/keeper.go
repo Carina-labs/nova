596de1b60f734b7d868bd764faf6734355c495f1
boosik@a41ventures.com <boosik@a41ventures.com>
2022-05-02 23:52:34 +0900

    wip: implemenets gal module

--- x/gal/keeper/keeper.go
+++ x/gal/keeper/keeper.go
@@ -1,18 +1,49 @@
 package keeper
 
-import sdk "github.com/cosmos/cosmos-sdk/types"
+import (
+	"github.com/Carina-labs/novachain/x/gal/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	"github.com/tendermint/tendermint/libs/log"
+)
 
 // Keeper defines a module interface that facilitates the transfer of coins between accounts.
-type Keeper interface {
-	InitGenesis(sdk.Context, *types.GenesisState)
-	ExportGenesis(sdk.Context) *types.GenesisState
+type Keeper struct {
+	cdc        codec.BinaryCodec
+	storeKey   sdk.StoreKey
+	paramSpace paramtypes.Subspace
+	bankKeeper types.BankKeeper
 }
 
-// BaseKeeper manages transfers between accounts. It implements the Keeper interface.
-type BaseKeeper struct {
+func NewKeeper(cdc codec.BinaryCodec, key sdk.StoreKey, paramSpace paramtypes.Subspace, bk types.BankKeeper) Keeper {
+	return Keeper{
+		cdc:        cdc,
+		storeKey:   key,
+		bankKeeper: bk,
+	}
 }
 
-func NewBaseKeeper() BaseKeeper {
-	// TODO : implements this!
-	return BaseKeeper{}
+// Logger returns a module-specific logger.
+func (k Keeper) Logger(ctx sdk.Context) log.Logger {
+	return ctx.Logger().With("module", "x/"+types.ModuleName)
+}
+
+// SetParams sets the total set of gal parameters.
+func (k Keeper) SetParams(ctx sdk.Context, params *types.Params) {
+	k.paramSpace.SetParamSet(ctx, params)
+}
+
+// GetParams returns total set of gal parameters.
+func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
+	k.paramSpace.GetParamSet(ctx, &params)
+	return params
+}
+
+func (k Keeper) DepositNativeToken() {
+
+}
+
+func (k Keeper) WithdrawNovaToken() {
+	
 }
