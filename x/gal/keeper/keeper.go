b5ef4f29423d9badd56fe095da718831e8885a95
boosik@a41ventures.com <boosik@a41ventures.com>
2022-05-04 17:25:01 +0900

    wip: define msg server interfaces

--- x/gal/keeper/keeper.go
+++ x/gal/keeper/keeper.go
@@ -4,23 +4,35 @@ import (
 	"github.com/Carina-labs/novachain/x/gal/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	capabilitykeeper "github.com/cosmos/cosmos-sdk/x/capability/keeper"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	icacontrollerkeeper "github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts/controller/keeper"
 	"github.com/tendermint/tendermint/libs/log"
 )
 
 // Keeper defines a module interface that facilitates the transfer of coins between accounts.
 type Keeper struct {
-	cdc        codec.BinaryCodec
-	storeKey   sdk.StoreKey
-	paramSpace paramtypes.Subspace
-	bankKeeper types.BankKeeper
+	cdc                 codec.BinaryCodec
+	storeKey            sdk.StoreKey
+	paramSpace          paramtypes.Subspace
+	bankKeeper          types.BankKeeper
+	scopedKeeper        capabilitykeeper.ScopedKeeper
+	icaControllerKeeper icacontrollerkeeper.Keeper
 }
 
-func NewKeeper(cdc codec.BinaryCodec, key sdk.StoreKey, paramSpace paramtypes.Subspace, bk types.BankKeeper) Keeper {
+func NewKeeper(cdc codec.BinaryCodec,
+	key sdk.StoreKey,
+	paramSpace paramtypes.Subspace,
+	bankKeeper types.BankKeeper,
+	scopedKeeper capabilitykeeper.ScopedKeeper,
+	iaKeeper icacontrollerkeeper.Keeper) Keeper {
 	return Keeper{
-		cdc:        cdc,
-		storeKey:   key,
-		bankKeeper: bk,
+		cdc:                 cdc,
+		storeKey:            key,
+		bankKeeper:          bankKeeper,
+		paramSpace:          paramSpace,
+		scopedKeeper:        scopedKeeper,
+		icaControllerKeeper: iaKeeper,
 	}
 }
 
@@ -42,30 +54,30 @@ func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
 
 func (k Keeper) DepositNativeToken(ctx sdk.Context, depositor string, amt sdk.Coins) {
 	// wAtom -> [ GAL ] -> snAtom
-	for _, coin := range amt {
-		// mint new sn token
-		if err := k.bankKeeper.MintCoins(ctx, types.ModuleName, sdk.Coins{sdk.Coin{}}); err != nil {
-
-		}
-
-		// burn wrapped token
-		if err := k.bankKeeper.BurnCoins(ctx, types.ModuleName, sdk.Coins{sdk.Coin{}}); err != nil {
-			
-		}
-	}
+	//for _, coin := range amt {
+	//	// mint new sn token
+	//	if err := k.bankKeeper.MintCoins(ctx, types.ModuleName, sdk.Coins{sdk.Coin{}}); err != nil {
+	//
+	//	}
+	//
+	//	// burn wrapped token
+	//	if err := k.bankKeeper.BurnCoins(ctx, types.ModuleName, sdk.Coins{sdk.Coin{}}); err != nil {
+	//
+	//	}
+	//}
 }
 
 func (k Keeper) WithdrawNovaToken(ctx sdk.Context, withdrawer string, amt sdk.Coins) {
 	// snAtom -> [GAL] -> wAtom
-	for _, coin := range amt {
-		// burn sn token
-		if err := k.bankKeeper.BurnCoins(ctx, types.ModuleName, sdk.Coins{sdk.Coin{}}); err != nil {
-
-		}
-
-		// mint new w token
-		if err := k.bankKeeper.MintCoins(ctx, types.ModuleName, sdk.Coins{sdk.Coin{}}); err != nil {
-
-		}
-	}
+	//for _, coin := range amt {
+	//	// burn sn token
+	//	if err := k.bankKeeper.BurnCoins(ctx, types.ModuleName, sdk.Coins{sdk.Coin{}}); err != nil {
+	//
+	//	}
+	//
+	//	// mint new w token
+	//	if err := k.bankKeeper.MintCoins(ctx, types.ModuleName, sdk.Coins{sdk.Coin{}}); err != nil {
+	//
+	//	}
+	//}
 }
