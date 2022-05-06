3e6e8f52cd9b4b1c28fda539d80c6a5a2630b523
boosik@a41ventures.com <boosik@a41ventures.com>
2022-05-06 18:39:50 +0900

    fix: protogen script using buf

--- scripts/protogen.sh
+++ scripts/protogen.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-buf generate
+#buf generate
+#
+#cp -r ./gen/proto/go/novachain/gal/v1/* ./x/gal/types
+#rm -rf gen
 
-cp -r ./gen/proto/go/novachain/gal/v1/* ./x/gal/types
-rm -rf gen
+set -eo pipefail
+
+echo "Generating gogo proto code"
+cd proto
+proto_dirs=$(find ./novachain -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+    if grep "option go_package" $file &> /dev/null ; then
+      buf generate --template buf.gen.gogo.yaml $file
+    fi
+  done
+done
+
+cd ..
+
+cp -r github.com/Carina-labs/novachain/* ./
+rm -rf github.com
+
+go mod tidy -compat=1.18
