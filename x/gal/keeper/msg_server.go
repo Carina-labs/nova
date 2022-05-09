1671f29954ff091e9bfe68a39c5bb3515740f9d1
boosik@a41ventures.com <boosik@a41ventures.com>
2022-05-09 16:47:43 +0900

    wip: query cmd and proto

--- x/gal/keeper/msg_server.go
+++ x/gal/keeper/msg_server.go
@@ -3,20 +3,34 @@ package keeper
 import (
 	"context"
 	"github.com/Carina-labs/novachain/x/gal/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 var _ types.MsgServer = msgServer{}
 
 type msgServer struct {
-	Keeper
+	keeper Keeper
 }
 
-func (k msgServer) Deposit(ctx context.Context, deposit *types.MsgDeposit) (*types.MsgDepositResponse, error) {
-	//TODO implement me
-	panic("implement me")
+func (m msgServer) Deposit(goCtx context.Context, deposit *types.MsgDeposit) (*types.MsgDepositResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	if err := m.keeper.DepositNativeToken(ctx, deposit.Depositor, deposit.Amount); err != nil {
+		return nil, err
+	}
+	return &types.MsgDepositResponse{}, nil
+}
+
+func (m msgServer) Withdraw(goCtx context.Context, withdraw *types.MsgWithdraw) (*types.MsgWithdrawResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	if err := m.keeper.WithdrawNovaToken(ctx, withdraw.Withdrawer, withdraw.Amount); err != nil {
+		return nil, err
+	}
+	return &types.MsgWithdrawResponse{}, nil
 }
 
 // NewMsgServerImpl creates and returns a new types.MsgServer, fulfilling the intertx Msg service interface
 func NewMsgServerImpl(keeper Keeper) types.MsgServer {
-	return &msgServer{Keeper: keeper}
+	return msgServer{keeper: keeper}
 }
--- x/gal/types/msgs.go
+++ x/gal/types/msgs.go
@@ -1,1 +1,95 @@
 package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+const (
+	TypeMsgDeposit  = "deposit"
+	TypeMsgWithdraw = "withdraw"
+)
+
+var _ sdk.Msg = &MsgDeposit{}
+
+func NewMsgDeposit(fromAddr sdk.AccAddress, amount sdk.Coins) *MsgDeposit {
+	return &MsgDeposit{
+		Depositor: fromAddr.String(),
+		Amount:    amount,
+	}
+}
+
+func (msg MsgDeposit) Route() string {
+	return RouterKey
+}
+
+func (msg MsgDeposit) Type() string {
+	return TypeMsgDeposit
+}
+
+func (msg MsgDeposit) ValidateBasic() error {
+	if _, err := sdk.AccAddressFromBech32(msg.Depositor); err != nil {
+		return err
+	}
+
+	if !msg.Amount.IsValid() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, msg.Amount.String())
+	}
+
+	if !msg.Amount.IsAllPositive() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, msg.Amount.String())
+	}
+
+	return nil
+}
+
+func (msg MsgDeposit) GetSignBytes() []byte {
+	return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(&msg))
+}
+
+func (msg MsgDeposit) GetSigners() []sdk.AccAddress {
+	depositor, _ := sdk.AccAddressFromBech32(msg.Depositor)
+	return []sdk.AccAddress{depositor}
+}
+
+var _ sdk.Msg = &MsgWithdraw{}
+
+func NewMsgWithdraw(toAddr sdk.AccAddress, amount sdk.Coins) *MsgWithdraw {
+	return &MsgWithdraw{
+		Withdrawer: toAddr.String(),
+		Amount:     amount,
+	}
+}
+
+func (msg MsgWithdraw) Route() string {
+	return RouterKey
+}
+
+func (msg MsgWithdraw) Type() string {
+	return TypeMsgWithdraw
+}
+
+func (msg MsgWithdraw) ValidateBasic() error {
+	if _, err := sdk.AccAddressFromBech32(msg.Withdrawer); err != nil {
+		return err
+	}
+
+	if !msg.Amount.IsValid() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, msg.Amount.String())
+	}
+
+	if !msg.Amount.IsAllPositive() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, msg.Amount.String())
+	}
+
+	return nil
+}
+
+func (msg MsgWithdraw) GetSignBytes() []byte {
+	return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(&msg))
+}
+
+func (msg MsgWithdraw) GetSigners() []sdk.AccAddress {
+	withdrawer, _ := sdk.AccAddressFromBech32(msg.Withdrawer)
+	return []sdk.AccAddress{withdrawer}
+}
