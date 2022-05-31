063a47dc30f5ebeb1bf8fc3c15c9b570d8720ce2
boosik@a41ventures.com <boosik@a41ventures.com>
2022-05-31 15:14:02 +0900

    fix: delete unused value in proto

--- x/oracle/types/genesis.pb.go
+++ x/oracle/types/genesis.pb.go
@@ -67,10 +67,11 @@ func (m *GenesisState) GetStates() []*ChainInfo {
 }
 
 type ChainInfo struct {
-	ChainDenom       string                `protobuf:"bytes,1,opt,name=chain_denom,json=chainDenom,proto3" json:"chain_denom,omitempty"`
-	ValidatorAddress string                `protobuf:"bytes,2,opt,name=validator_address,json=validatorAddress,proto3" json:"validator_address,omitempty"`
-	LastBlockHeight  int64                 `protobuf:"varint,3,opt,name=last_block_height,json=lastBlockHeight,proto3" json:"last_block_height,omitempty"`
-	StateMap         map[int64]*ChainState `protobuf:"bytes,4,rep,name=state_map,json=stateMap,proto3" json:"state_map,omitempty" protobuf_key:"varint,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
+	ChainDenom         string `protobuf:"bytes,1,opt,name=chain_denom,json=chainDenom,proto3" json:"chain_denom,omitempty"`
+	ValidatorAddress   string `protobuf:"bytes,2,opt,name=validator_address,json=validatorAddress,proto3" json:"validator_address,omitempty"`
+	LastBlockHeight    int64  `protobuf:"varint,3,opt,name=last_block_height,json=lastBlockHeight,proto3" json:"last_block_height,omitempty"`
+	TotalStakedBalance int64  `protobuf:"varint,5,opt,name=total_staked_balance,json=totalStakedBalance,proto3" json:"total_staked_balance,omitempty"`
+	Decimal            int64  `protobuf:"varint,6,opt,name=decimal,proto3" json:"decimal,omitempty"`
 }
 
 func (m *ChainInfo) Reset()         { *m = ChainInfo{} }
@@ -127,59 +128,14 @@ func (m *ChainInfo) GetLastBlockHeight() int64 {
 	return 0
 }
 
-func (m *ChainInfo) GetStateMap() map[int64]*ChainState {
-	if m != nil {
-		return m.StateMap
-	}
-	return nil
-}
-
-type ChainState struct {
-	TotalStakedBalance int64 `protobuf:"varint,1,opt,name=total_staked_balance,json=totalStakedBalance,proto3" json:"total_staked_balance,omitempty"`
-	Decimal            int64 `protobuf:"varint,2,opt,name=decimal,proto3" json:"decimal,omitempty"`
-}
-
-func (m *ChainState) Reset()         { *m = ChainState{} }
-func (m *ChainState) String() string { return proto.CompactTextString(m) }
-func (*ChainState) ProtoMessage()    {}
-func (*ChainState) Descriptor() ([]byte, []int) {
-	return fileDescriptor_3cbadcf840d1eb62, []int{2}
-}
-func (m *ChainState) XXX_Unmarshal(b []byte) error {
-	return m.Unmarshal(b)
-}
-func (m *ChainState) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	if deterministic {
-		return xxx_messageInfo_ChainState.Marshal(b, m, deterministic)
-	} else {
-		b = b[:cap(b)]
-		n, err := m.MarshalToSizedBuffer(b)
-		if err != nil {
-			return nil, err
-		}
-		return b[:n], nil
-	}
-}
-func (m *ChainState) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_ChainState.Merge(m, src)
-}
-func (m *ChainState) XXX_Size() int {
-	return m.Size()
-}
-func (m *ChainState) XXX_DiscardUnknown() {
-	xxx_messageInfo_ChainState.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_ChainState proto.InternalMessageInfo
-
-func (m *ChainState) GetTotalStakedBalance() int64 {
+func (m *ChainInfo) GetTotalStakedBalance() int64 {
 	if m != nil {
 		return m.TotalStakedBalance
 	}
 	return 0
 }
 
-func (m *ChainState) GetDecimal() int64 {
+func (m *ChainInfo) GetDecimal() int64 {
 	if m != nil {
 		return m.Decimal
 	}
@@ -189,39 +145,32 @@ func (m *ChainState) GetDecimal() int64 {
 func init() {
 	proto.RegisterType((*GenesisState)(nil), "novachain.oracle.v1.GenesisState")
 	proto.RegisterType((*ChainInfo)(nil), "novachain.oracle.v1.ChainInfo")
-	proto.RegisterMapType((map[int64]*ChainState)(nil), "novachain.oracle.v1.ChainInfo.StateMapEntry")
-	proto.RegisterType((*ChainState)(nil), "novachain.oracle.v1.ChainState")
 }
 
 func init() { proto.RegisterFile("novachain/oracle/v1/genesis.proto", fileDescriptor_3cbadcf840d1eb62) }
 
 var fileDescriptor_3cbadcf840d1eb62 = []byte{
-	// 398 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x7c, 0x92, 0xcd, 0xae, 0xd2, 0x40,
-	0x14, 0xc7, 0x19, 0xaa, 0x28, 0x07, 0x8d, 0x30, 0xba, 0x68, 0x5c, 0x14, 0x64, 0x45, 0xfc, 0x68,
-	0x05, 0xa3, 0x31, 0xee, 0x04, 0xbf, 0x58, 0xb8, 0x29, 0x1b, 0x63, 0x4c, 0x9a, 0xd3, 0x76, 0xa4,
-	0x0d, 0xd3, 0x99, 0xa6, 0x33, 0x34, 0xf2, 0x16, 0x3e, 0x89, 0xcf, 0xe1, 0x92, 0xa5, 0x4b, 0x03,
-	0x2f, 0x72, 0xd3, 0x29, 0x1f, 0xb9, 0xc9, 0xbd, 0x77, 0x37, 0xe7, 0xff, 0xfb, 0x9f, 0x39, 0xe7,
-	0x9f, 0x1c, 0x78, 0x22, 0x64, 0x89, 0x51, 0x82, 0xa9, 0xf0, 0x64, 0x81, 0x11, 0x67, 0x5e, 0x39,
-	0xf6, 0x96, 0x4c, 0x30, 0x95, 0x2a, 0x37, 0x2f, 0xa4, 0x96, 0xf4, 0xe1, 0xc9, 0xe2, 0xd6, 0x16,
-	0xb7, 0x1c, 0x0f, 0x3f, 0xc1, 0xbd, 0xcf, 0xb5, 0x6b, 0xa1, 0x51, 0x33, 0xfa, 0x06, 0x5a, 0xaa,
-	0x7a, 0x28, 0x9b, 0x0c, 0xac, 0x51, 0x67, 0xe2, 0xb8, 0x57, 0x74, 0xb9, 0xb3, 0xaa, 0x9e, 0x8b,
-	0x9f, 0xd2, 0x3f, 0xb8, 0x87, 0x7f, 0x9a, 0xd0, 0x3e, 0xa9, 0xb4, 0x0f, 0x1d, 0xd3, 0x12, 0xc4,
-	0x4c, 0xc8, 0xcc, 0x26, 0x03, 0x32, 0x6a, 0xfb, 0x60, 0xa4, 0x0f, 0x95, 0x42, 0x9f, 0x41, 0xaf,
-	0x44, 0x9e, 0xc6, 0xa8, 0x65, 0x11, 0x60, 0x1c, 0x17, 0x4c, 0x29, 0xbb, 0x69, 0x6c, 0xdd, 0x13,
-	0x78, 0x5f, 0xeb, 0xf4, 0x29, 0xf4, 0x38, 0x2a, 0x1d, 0x84, 0x5c, 0x46, 0xab, 0x20, 0x61, 0xe9,
-	0x32, 0xd1, 0xb6, 0x35, 0x20, 0x23, 0xcb, 0x7f, 0x50, 0x81, 0x69, 0xa5, 0x7f, 0x31, 0x32, 0x9d,
-	0x43, 0xdb, 0x6c, 0x14, 0x64, 0x98, 0xdb, 0xb7, 0x4c, 0x84, 0xe7, 0x37, 0x47, 0x70, 0x4d, 0xf0,
-	0xaf, 0x98, 0x7f, 0x14, 0xba, 0xd8, 0xf8, 0x77, 0xd5, 0xa1, 0x7c, 0xfc, 0x03, 0xee, 0x5f, 0x42,
-	0xb4, 0x0b, 0xd6, 0x8a, 0x6d, 0x4c, 0x1a, 0xcb, 0xaf, 0x9e, 0xf4, 0x35, 0xdc, 0x2e, 0x91, 0xaf,
-	0x99, 0x59, 0xbd, 0x33, 0xe9, 0x5f, 0x3f, 0xc9, 0xfc, 0xe4, 0xd7, 0xee, 0x77, 0xcd, 0xb7, 0x64,
-	0xf8, 0x0d, 0xe0, 0x0c, 0xe8, 0x4b, 0x78, 0xa4, 0xa5, 0x46, 0x1e, 0x28, 0x8d, 0x2b, 0x16, 0x07,
-	0x21, 0x72, 0x14, 0x11, 0x3b, 0xcc, 0xa2, 0x86, 0x2d, 0x0c, 0x9a, 0xd6, 0x84, 0xda, 0x70, 0x27,
-	0x66, 0x51, 0x9a, 0x21, 0x37, 0xc3, 0x2d, 0xff, 0x58, 0x4e, 0xe7, 0x7f, 0x77, 0x0e, 0xd9, 0xee,
-	0x1c, 0xf2, 0x7f, 0xe7, 0x90, 0xdf, 0x7b, 0xa7, 0xb1, 0xdd, 0x3b, 0x8d, 0x7f, 0x7b, 0xa7, 0xf1,
-	0xdd, 0x5b, 0xa6, 0x3a, 0x59, 0x87, 0x6e, 0x24, 0x33, 0x6f, 0x86, 0x45, 0x2a, 0xf0, 0x05, 0xc7,
-	0x50, 0x79, 0xe7, 0xdb, 0xf9, 0x75, 0xbc, 0x1e, 0xbd, 0xc9, 0x99, 0x0a, 0x5b, 0xe6, 0x72, 0x5e,
-	0x5d, 0x04, 0x00, 0x00, 0xff, 0xff, 0x05, 0x63, 0x1a, 0xde, 0x5e, 0x02, 0x00, 0x00,
+	// 311 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x91, 0xb1, 0x4e, 0x02, 0x31,
+	0x1c, 0x87, 0xa9, 0x44, 0x0c, 0xc5, 0x44, 0xa9, 0x0e, 0x9d, 0x2a, 0x32, 0x11, 0x8d, 0x77, 0xa2,
+	0x89, 0xbb, 0x60, 0x54, 0x56, 0xd8, 0x5c, 0x2e, 0xff, 0x6b, 0x2b, 0xd7, 0x50, 0x5a, 0x72, 0xad,
+	0x17, 0x7d, 0x0b, 0x1f, 0xcb, 0x11, 0x37, 0x47, 0x03, 0x2f, 0x62, 0xae, 0x27, 0xb8, 0xb8, 0xb5,
+	0xdf, 0xf7, 0xfd, 0xa7, 0x1f, 0x3e, 0x35, 0xb6, 0x00, 0x9e, 0x81, 0x32, 0xb1, 0xcd, 0x81, 0x6b,
+	0x19, 0x17, 0xfd, 0x78, 0x2a, 0x8d, 0x74, 0xca, 0x45, 0x8b, 0xdc, 0x7a, 0x4b, 0x8e, 0xb6, 0x49,
+	0x54, 0x25, 0x51, 0xd1, 0xef, 0xde, 0xe3, 0xfd, 0x87, 0xaa, 0x9a, 0x78, 0xf0, 0x92, 0xdc, 0xe0,
+	0x86, 0x2b, 0x1f, 0x8e, 0xa2, 0x4e, 0xbd, 0xd7, 0xba, 0x62, 0xd1, 0x3f, 0x57, 0xd1, 0xb0, 0xfc,
+	0x8f, 0xcc, 0xb3, 0x1d, 0xff, 0xd6, 0xdd, 0x4f, 0x84, 0x9b, 0x5b, 0x4a, 0x4e, 0x70, 0x2b, 0x9c,
+	0x24, 0x42, 0x1a, 0x3b, 0xa7, 0xa8, 0x83, 0x7a, 0xcd, 0x31, 0x0e, 0xe8, 0xae, 0x24, 0xe4, 0x1c,
+	0xb7, 0x0b, 0xd0, 0x4a, 0x80, 0xb7, 0x79, 0x02, 0x42, 0xe4, 0xd2, 0x39, 0xba, 0x13, 0xb2, 0xc3,
+	0xad, 0xb8, 0xad, 0x38, 0x39, 0xc3, 0x6d, 0x0d, 0xce, 0x27, 0xa9, 0xb6, 0x7c, 0x96, 0x64, 0x52,
+	0x4d, 0x33, 0x4f, 0xeb, 0x1d, 0xd4, 0xab, 0x8f, 0x0f, 0x4a, 0x31, 0x28, 0xf9, 0x63, 0xc0, 0xe4,
+	0x12, 0x1f, 0x7b, 0xeb, 0x41, 0x27, 0xce, 0xc3, 0x4c, 0x8a, 0x24, 0x05, 0x0d, 0x86, 0x4b, 0xba,
+	0x1b, 0x72, 0x12, 0xdc, 0x24, 0xa8, 0x41, 0x65, 0x08, 0xc5, 0x7b, 0x42, 0x72, 0x35, 0x07, 0x4d,
+	0x1b, 0x21, 0xda, 0x7c, 0x07, 0xa3, 0x8f, 0x15, 0x43, 0xcb, 0x15, 0x43, 0xdf, 0x2b, 0x86, 0xde,
+	0xd7, 0xac, 0xb6, 0x5c, 0xb3, 0xda, 0xd7, 0x9a, 0xd5, 0x9e, 0xe2, 0xa9, 0xf2, 0xd9, 0x4b, 0x1a,
+	0x71, 0x3b, 0x8f, 0x87, 0x90, 0x2b, 0x03, 0x17, 0x1a, 0x52, 0x17, 0xff, 0x8d, 0xf0, 0xba, 0x99,
+	0xc1, 0xbf, 0x2d, 0xa4, 0x4b, 0x1b, 0x61, 0x82, 0xeb, 0x9f, 0x00, 0x00, 0x00, 0xff, 0xff, 0x40,
+	0xf1, 0xee, 0x37, 0xa7, 0x01, 0x00, 0x00,
 }
 
 func (m *GenesisState) Marshal() (dAtA []byte, err error) {
@@ -281,29 +230,15 @@ func (m *ChainInfo) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
-	if len(m.StateMap) > 0 {
-		for k := range m.StateMap {
-			v := m.StateMap[k]
-			baseI := i
-			if v != nil {
-				{
-					size, err := v.MarshalToSizedBuffer(dAtA[:i])
-					if err != nil {
-						return 0, err
-					}
-					i -= size
-					i = encodeVarintGenesis(dAtA, i, uint64(size))
-				}
-				i--
-				dAtA[i] = 0x12
-			}
-			i = encodeVarintGenesis(dAtA, i, uint64(k))
-			i--
-			dAtA[i] = 0x8
-			i = encodeVarintGenesis(dAtA, i, uint64(baseI-i))
-			i--
-			dAtA[i] = 0x22
-		}
+	if m.Decimal != 0 {
+		i = encodeVarintGenesis(dAtA, i, uint64(m.Decimal))
+		i--
+		dAtA[i] = 0x30
+	}
+	if m.TotalStakedBalance != 0 {
+		i = encodeVarintGenesis(dAtA, i, uint64(m.TotalStakedBalance))
+		i--
+		dAtA[i] = 0x28
 	}
 	if m.LastBlockHeight != 0 {
 		i = encodeVarintGenesis(dAtA, i, uint64(m.LastBlockHeight))
@@ -323,39 +258,6 @@ func (m *ChainInfo) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i = encodeVarintGenesis(dAtA, i, uint64(len(m.ChainDenom)))
 		i--
 		dAtA[i] = 0xa
-	}
-	return len(dAtA) - i, nil
-}
-
-func (m *ChainState) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalToSizedBuffer(dAtA[:size])
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *ChainState) MarshalTo(dAtA []byte) (int, error) {
-	size := m.Size()
-	return m.MarshalToSizedBuffer(dAtA[:size])
-}
-
-func (m *ChainState) MarshalToSizedBuffer(dAtA []byte) (int, error) {
-	i := len(dAtA)
-	_ = i
-	var l int
-	_ = l
-	if m.Decimal != 0 {
-		i = encodeVarintGenesis(dAtA, i, uint64(m.Decimal))
-		i--
-		dAtA[i] = 0x10
-	}
-	if m.TotalStakedBalance != 0 {
-		i = encodeVarintGenesis(dAtA, i, uint64(m.TotalStakedBalance))
-		i--
-		dAtA[i] = 0x8
 	}
 	return len(dAtA) - i, nil
 }
@@ -403,28 +305,6 @@ func (m *ChainInfo) Size() (n int) {
 	if m.LastBlockHeight != 0 {
 		n += 1 + sovGenesis(uint64(m.LastBlockHeight))
 	}
-	if len(m.StateMap) > 0 {
-		for k, v := range m.StateMap {
-			_ = k
-			_ = v
-			l = 0
-			if v != nil {
-				l = v.Size()
-				l += 1 + sovGenesis(uint64(l))
-			}
-			mapEntrySize := 1 + sovGenesis(uint64(k)) + l
-			n += mapEntrySize + 1 + sovGenesis(uint64(mapEntrySize))
-		}
-	}
-	return n
-}
-
-func (m *ChainState) Size() (n int) {
-	if m == nil {
-		return 0
-	}
-	var l int
-	_ = l
 	if m.TotalStakedBalance != 0 {
 		n += 1 + sovGenesis(uint64(m.TotalStakedBalance))
 	}
@@ -636,172 +516,7 @@ func (m *ChainInfo) Unmarshal(dAtA []byte) error {
 					break
 				}
 			}
-		case 4:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field StateMap", wireType)
-			}
-			var msglen int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowGenesis
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				msglen |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			if msglen < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			postIndex := iNdEx + msglen
-			if postIndex < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			if m.StateMap == nil {
-				m.StateMap = make(map[int64]*ChainState)
-			}
-			var mapkey int64
-			var mapvalue *ChainState
-			for iNdEx < postIndex {
-				entryPreIndex := iNdEx
-				var wire uint64
-				for shift := uint(0); ; shift += 7 {
-					if shift >= 64 {
-						return ErrIntOverflowGenesis
-					}
-					if iNdEx >= l {
-						return io.ErrUnexpectedEOF
-					}
-					b := dAtA[iNdEx]
-					iNdEx++
-					wire |= uint64(b&0x7F) << shift
-					if b < 0x80 {
-						break
-					}
-				}
-				fieldNum := int32(wire >> 3)
-				if fieldNum == 1 {
-					for shift := uint(0); ; shift += 7 {
-						if shift >= 64 {
-							return ErrIntOverflowGenesis
-						}
-						if iNdEx >= l {
-							return io.ErrUnexpectedEOF
-						}
-						b := dAtA[iNdEx]
-						iNdEx++
-						mapkey |= int64(b&0x7F) << shift
-						if b < 0x80 {
-							break
-						}
-					}
-				} else if fieldNum == 2 {
-					var mapmsglen int
-					for shift := uint(0); ; shift += 7 {
-						if shift >= 64 {
-							return ErrIntOverflowGenesis
-						}
-						if iNdEx >= l {
-							return io.ErrUnexpectedEOF
-						}
-						b := dAtA[iNdEx]
-						iNdEx++
-						mapmsglen |= int(b&0x7F) << shift
-						if b < 0x80 {
-							break
-						}
-					}
-					if mapmsglen < 0 {
-						return ErrInvalidLengthGenesis
-					}
-					postmsgIndex := iNdEx + mapmsglen
-					if postmsgIndex < 0 {
-						return ErrInvalidLengthGenesis
-					}
-					if postmsgIndex > l {
-						return io.ErrUnexpectedEOF
-					}
-					mapvalue = &ChainState{}
-					if err := mapvalue.Unmarshal(dAtA[iNdEx:postmsgIndex]); err != nil {
-						return err
-					}
-					iNdEx = postmsgIndex
-				} else {
-					iNdEx = entryPreIndex
-					skippy, err := skipGenesis(dAtA[iNdEx:])
-					if err != nil {
-						return err
-					}
-					if (skippy < 0) || (iNdEx+skippy) < 0 {
-						return ErrInvalidLengthGenesis
-					}
-					if (iNdEx + skippy) > postIndex {
-						return io.ErrUnexpectedEOF
-					}
-					iNdEx += skippy
-				}
-			}
-			m.StateMap[mapkey] = mapvalue
-			iNdEx = postIndex
-		default:
-			iNdEx = preIndex
-			skippy, err := skipGenesis(dAtA[iNdEx:])
-			if err != nil {
-				return err
-			}
-			if (skippy < 0) || (iNdEx+skippy) < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
-}
-func (m *ChainState) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowGenesis
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= uint64(b&0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: ChainState: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: ChainState: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
+		case 5:
 			if wireType != 0 {
 				return fmt.Errorf("proto: wrong wireType = %d for field TotalStakedBalance", wireType)
 			}
@@ -820,7 +535,7 @@ func (m *ChainState) Unmarshal(dAtA []byte) error {
 					break
 				}
 			}
-		case 2:
+		case 6:
 			if wireType != 0 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Decimal", wireType)
 			}
