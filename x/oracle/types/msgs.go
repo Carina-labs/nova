5b0e5c418217220a2a343c063ed769c53b00b897
boosik@a41ventures.com <boosik@a41ventures.com>
2022-05-31 19:13:12 +0900

    fix: basic params in keeper

--- x/oracle/types/msgs.go
+++ x/oracle/types/msgs.go
@@ -7,14 +7,12 @@ var _ sdk.Msg = &MsgUpdateChainState{}
 func NewMsgUpdateChainState(chainDenom string,
 	balance uint64,
 	decimal uint64,
-	blockHeight uint64,
-	proof []byte) *MsgUpdateChainState {
+	blockHeight uint64) *MsgUpdateChainState {
 	return &MsgUpdateChainState{
 		ChainDenom:    chainDenom,
 		StakedBalance: balance,
 		Decimal:       decimal,
 		BlockHeight:   blockHeight,
-		Proof:         proof,
 	}
 }
 
