f280bd4e2731727a141d0693280a2932f956da96
boosik <boosik@a41ventures.com>
2022-06-04 20:20:47 +0900

    feat: implements GetChainState()

--- x/oracle/keeper/keeper.go
+++ x/oracle/keeper/keeper.go
@@ -2,6 +2,8 @@ package keeper
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/Carina-labs/nova/x/oracle/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -35,7 +37,7 @@ func (k Keeper) UpdateChainState(ctx sdk.Context, updateInfo *types.MsgUpdateCha
 	}
 
 	store := ctx.KVStore(k.storeKey)
-	data := make(map[string]interface{})
+	data := make(map[string]uint64)
 	data["balance"] = updateInfo.StakedBalance
 	data["decimal"] = updateInfo.Decimal
 	data["height"] = updateInfo.BlockHeight
@@ -48,6 +50,22 @@ func (k Keeper) UpdateChainState(ctx sdk.Context, updateInfo *types.MsgUpdateCha
 	return nil
 }
 
-func (k Keeper) GetChainState(chainDenom string) *types.QueryStateResponse {
-	return &types.QueryStateResponse{}
+func (k Keeper) GetChainState(ctx sdk.Context, chainDenom string) (*types.QueryStateResponse, error) {
+	store := ctx.KVStore(k.storeKey)
+	if !store.Has([]byte(chainDenom)) {
+		// TODO : aggregate errors to types
+		return nil, errors.New(fmt.Sprintf("%s is not supported.", chainDenom))
+	}
+
+	result := make(map[string]uint64)
+	err := json.Unmarshal(store.Get([]byte(chainDenom)), &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.QueryStateResponse{
+		TotalStakedBalance: result["balance"],
+		Decimal:            result["decimal"],
+		LastBlockHeight:    result["height"],
+	}, nil
 }
