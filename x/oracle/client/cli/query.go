85d61f68e42926602f599779f94f206f26746636
boosik <boosik@a41ventures.com>
2022-06-04 17:07:57 +0900

    fix: mistypo import

--- x/oracle/client/cli/query.go
+++ x/oracle/client/cli/query.go
@@ -1,7 +1,7 @@
 package cli
 
 import (
-	"github.com/Carina-labs/novachain/x/oracle/types"
+	"github.com/Carina-labs/nova/x/oracle/types"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/spf13/cobra"
 )
