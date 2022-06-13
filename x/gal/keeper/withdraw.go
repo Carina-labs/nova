3eac95582e5e6bc7310ae2be56bad3d5d598a7fb
boosik@a41ventures.com <boosik@a41ventures.com>
2022-06-14 01:34:10 +0900

    feat: implements receipt store and functions

--- x/gal/keeper/withdraw.go
+++ x/gal/keeper/withdraw.go
@@ -1,6 +1,7 @@
 package keeper
 
 import (
+	"fmt"
 	"github.com/Carina-labs/nova/x/gal/types"
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -20,14 +21,39 @@ func (k Keeper) DeleteWithdrawRecord(ctx sdk.Context) {
 
 }
 
-func (k Keeper) GetWithdrawReceipt(ctx sdk.Context) {
+func (k Keeper) GetWithdrawReceipt(ctx sdk.Context, key string) ([]byte, error) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyWithdrawReceiptInfo)
+	bk := []byte(key)
+	if !store.Has(bk) {
+		return nil, fmt.Errorf("The store does not have key %s", key)
+	}
 
+	return store.Get(bk), nil
 }
 
 func (k Keeper) SetWithdrawReceipt(ctx sdk.Context, receipt types.MsgWithdrawReceipt) {
-
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyWithdrawReceiptInfo)
+	bz := k.cdc.MustMarshal(&receipt)
+	store.Set([]byte(receipt.ZoneId+receipt.Withdrawer), bz)
 }
 
 func (k Keeper) DeleteWithdrawReceipt(ctx sdk.Context) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyWithdrawReceiptInfo)
+	iterator := sdk.KVStorePrefixIterator(store, nil)
+	defer iterator.Close()
+	for ; iterator.Valid(); iterator.Next() {
+		key := iterator.Key()
+		store.Delete(key)
+	}
+}
 
+func (k Keeper) DeleteWithdrawReceiptItem(ctx sdk.Context, key string) error {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyWithdrawReceiptInfo)
+	bk := []byte(key)
+	if !store.Has(bk) {
+		return fmt.Errorf("The store does not have key %s", key)
+	}
+
+	store.Delete(bk)
+	return nil
 }
