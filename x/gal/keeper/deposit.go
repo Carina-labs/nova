58df0c370c91d66e19e4716802ff8d9ab8bb328e
boosik@a41ventures.com <boosik@a41ventures.com>
2022-06-15 21:27:37 +0900

    chore: add comments for each functions

--- x/gal/keeper/deposit.go
+++ x/gal/keeper/deposit.go
@@ -8,18 +8,23 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-func (k Keeper) getDepositCache(ctx sdk.Context) prefix.Store {
+// getDepositRecordStore returns "DepositRecord" store.
+// It is used for finding the amount of coin user deposit.
+func (k Keeper) getDepositRecordStore(ctx sdk.Context) prefix.Store {
 	return prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyShare)
 }
 
-func (k Keeper) RecordDepositAmt(ctx sdk.Context, msg types.DepositRecord) {
-	store := k.getDepositCache(ctx)
+// RecordDepositAmt write the amount of coin user deposit to the "DepositRecord" store.
+func (k Keeper) RecordDepositAmt(ctx sdk.Context, msg types.DepositRecord) error {
+	store := k.getDepositRecordStore(ctx)
 	bz := k.cdc.MustMarshal(&msg)
 	store.Set([]byte(msg.Address), bz)
+	return nil
 }
 
+// GetRecordedDepositAmt returns the amount of coin user deposit by address.
 func (k Keeper) GetRecordedDepositAmt(ctx sdk.Context, depositor sdk.AccAddress) (*types.DepositRecord, error) {
-	store := k.getDepositCache(ctx)
+	store := k.getDepositRecordStore(ctx)
 	depositorStr := depositor.String()
 	if !store.Has([]byte(depositorStr)) {
 		return nil, fmt.Errorf("depositor %s is not in state", depositor)
@@ -32,8 +37,10 @@ func (k Keeper) GetRecordedDepositAmt(ctx sdk.Context, depositor sdk.AccAddress)
 	return &msg, nil
 }
 
+// ClearRecordedDepositAmt remove all data in "DepositRecord".
+// It must be removed after staking in host chain.
 func (k Keeper) ClearRecordedDepositAmt(ctx sdk.Context, depositor sdk.AccAddress) error {
-	store := k.getDepositCache(ctx)
+	store := k.getDepositRecordStore(ctx)
 	depositorStr := depositor.String()
 	if !store.Has([]byte(depositorStr)) {
 		return fmt.Errorf("depositor %s is not in state", depositor.String())
