800d89fc3cea542f9a7ff351f75c8b31d504e002
ellie <ellie@a41ventures.com>
2022-06-17 23:17:13 +0900

    feat: impl mint module

---
+++ x/mint/keeper/keeper.go
@@ -0,0 +1,191 @@
+package keeper
+
+import (
+	"fmt"
+
+	"github.com/tendermint/tendermint/libs/log"
+
+	"github.com/Carina-labs/nova/x/mint/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+)
+
+// Keeper of the mint store
+type Keeper struct {
+	cdc              codec.BinaryCodec
+	storeKey         sdk.StoreKey
+	paramSpace       paramtypes.Subspace
+	distrKeeper      types.DistrKeeper
+	stakingKeeper    types.StakingKeeper
+	accountKeeper    types.AccountKeeper
+	bankKeeper       types.BankKeeper
+	feeCollectorName string
+}
+
+// NewKeeper creates a new mint Keeper instance
+func NewKeeper(
+	cdc codec.BinaryCodec, key sdk.StoreKey, paramSpace paramtypes.Subspace,
+	sk types.StakingKeeper, ak types.AccountKeeper, bk types.BankKeeper, dk types.DistrKeeper,
+	feeCollectorName string,
+) Keeper {
+	// ensure mint module account is set
+	if addr := ak.GetModuleAddress(types.ModuleName); addr == nil {
+		panic("the mint module account has not been set")
+	}
+
+	// set KeyTable if it has not already been set
+	if !paramSpace.HasKeyTable() {
+		paramSpace = paramSpace.WithKeyTable(types.ParamKeyTable())
+	}
+
+	return Keeper{
+		cdc:              cdc,
+		storeKey:         key,
+		paramSpace:       paramSpace,
+		stakingKeeper:    sk,
+		accountKeeper:    ak,
+		bankKeeper:       bk,
+		distrKeeper:      dk,
+		feeCollectorName: feeCollectorName,
+	}
+}
+
+// CreateLpIncentiveModuleAccount creates the module account for developer vesting.
+// Should only be called in initial genesis creation, never again.
+func (k Keeper) CreateLpIncentiveModuleAccount(ctx sdk.Context, amount sdk.Coin) {
+	moduleAcc := authtypes.NewEmptyModuleAccount(
+		types.LpIncentiveModuleAccName, authtypes.Minter)
+
+	k.accountKeeper.SetModuleAccount(ctx, moduleAcc)
+
+	err := k.bankKeeper.MintCoins(ctx, types.LpIncentiveModuleAccName, sdk.NewCoins(amount))
+	if err != nil {
+		panic(err)
+	}
+}
+
+// CreateDeveloperVestingModuleAccount creates the module account for developer vesting.
+// Should only be called in initial genesis creation, never again.
+func (k Keeper) CreateStableGuaranteeIncentiveModuleAccount(ctx sdk.Context, amount sdk.Coin) {
+	moduleAcc := authtypes.NewEmptyModuleAccount(
+		types.StableGuaranteeIncentiveModuleAccName, authtypes.Minter)
+
+	k.accountKeeper.SetModuleAccount(ctx, moduleAcc)
+
+	err := k.bankKeeper.MintCoins(ctx, types.StableGuaranteeIncentiveModuleAccName, sdk.NewCoins(amount))
+	if err != nil {
+		panic(err)
+	}
+}
+
+// _____________________________________________________________________
+
+// Logger returns a module-specific logger.
+func (k Keeper) Logger(ctx sdk.Context) log.Logger {
+	return ctx.Logger().With("module", "x/"+types.ModuleName)
+}
+
+// get the minter
+func (k Keeper) GetMinter(ctx sdk.Context) (minter types.Minter) {
+	store := ctx.KVStore(k.storeKey)
+	b := store.Get(types.MinterKey)
+	if b == nil {
+		panic("stored minter should not have been nil")
+	}
+
+	k.cdc.MustUnmarshal(b, &minter)
+	return
+}
+
+// set the minter
+func (k Keeper) SetMinter(ctx sdk.Context, minter types.Minter) {
+	store := ctx.KVStore(k.storeKey)
+	b := k.cdc.MustMarshal(&minter)
+	store.Set(types.MinterKey, b)
+}
+
+// GetParams returns the total set of minting parameters.
+func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
+	k.paramSpace.GetParamSet(ctx, &params)
+	return params
+}
+
+// SetParams sets the total set of minting parameters.
+func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
+	k.paramSpace.SetParamSet(ctx, &params)
+}
+
+// StakingTokenSupply implements an alias call to the underlying staking keeper's
+// StakingTokenSupply to be used in BeginBlocker.
+func (k Keeper) StakingTokenSupply(ctx sdk.Context) sdk.Int {
+	return k.stakingKeeper.StakingTokenSupply(ctx)
+}
+
+// BondedRatio implements an alias call to the underlying staking keeper's
+// BondedRatio to be used in BeginBlocker.
+func (k Keeper) BondedRatio(ctx sdk.Context) sdk.Dec {
+	return k.stakingKeeper.BondedRatio(ctx)
+}
+
+// MintCoins implements an alias call to the underlying supply keeper's
+// MintCoins to be used in BeginBlocker.
+func (k Keeper) MintCoins(ctx sdk.Context, newCoins sdk.Coins) error {
+	if newCoins.Empty() {
+		// skip as no coins need to be minted
+		return nil
+	}
+
+	return k.bankKeeper.MintCoins(ctx, types.ModuleName, newCoins)
+}
+
+// AddCollectedFees implements an alias call to the underlying supply keeper's
+// AddCollectedFees to be used in BeginBlocker.
+func (k Keeper) AddCollectedFees(ctx sdk.Context, fees sdk.Coins) error {
+	return k.bankKeeper.SendCoinsFromModuleToModule(ctx, types.ModuleName, k.feeCollectorName, fees)
+}
+
+// GetProportions gets the balance of the `MintedDenom` from minted coins and returns coins according to the `AllocationRatio`.
+func (k Keeper) GetProportions(ctx sdk.Context, mintedCoin sdk.Coin, ratio sdk.Dec) sdk.Coin {
+	return sdk.NewCoin(mintedCoin.Denom, mintedCoin.Amount.ToDec().Mul(ratio).TruncateInt())
+}
+
+// DistributeMintedCoins implements distribution of minted coins from mint to external modules.
+func (k Keeper) DistributeMintedCoin(ctx sdk.Context, mintedCoin sdk.Coin) error {
+	params := k.GetParams(ctx)
+	proportions := params.DistributionProportions
+
+	// allocate staking incentives into fee collector account to be moved to on next begin blocker by staking module
+	stakingIncentivesCoins := sdk.NewCoins(k.GetProportions(ctx, mintedCoin, proportions.Staking))
+	err := k.bankKeeper.SendCoinsFromModuleToModule(ctx, types.ModuleName, k.feeCollectorName, stakingIncentivesCoins)
+	if err != nil {
+		return err
+	}
+	fmt.Println("stakingInentivesCoins : ", stakingIncentivesCoins)
+
+	lpIncentivesCoin := k.GetProportions(ctx, mintedCoin, proportions.LpIncentives)
+	fmt.Println("lpIncentivesCoin : ", lpIncentivesCoin)
+	lpIncentivesCoins := sdk.NewCoins(lpIncentivesCoin)
+	err = k.bankKeeper.SendCoinsFromModuleToModule(ctx, types.ModuleName, types.LpIncentiveModuleAccName, lpIncentivesCoins)
+	if err != nil {
+		return err
+	}
+
+	stableGuaranteeCoin := k.GetProportions(ctx, mintedCoin, proportions.StableGuaranteeIncentives)
+	stableGuaranteeCoins := sdk.NewCoins(stableGuaranteeCoin)
+	fmt.Println("stableGuaranteeCoins", stableGuaranteeCoins)
+	err = k.bankKeeper.SendCoinsFromModuleToModule(ctx, types.ModuleName, types.StableGuaranteeIncentiveModuleAccName, stableGuaranteeCoins)
+	if err != nil {
+		return err
+	}
+
+	communityPoolCoins := sdk.NewCoins(mintedCoin).Sub(stakingIncentivesCoins).Sub(lpIncentivesCoins).Sub(stableGuaranteeCoins)
+	fmt.Println("communityPoolCoins", communityPoolCoins)
+	err = k.distrKeeper.FundCommunityPool(ctx, communityPoolCoins, k.accountKeeper.GetModuleAddress(types.ModuleName))
+	if err != nil {
+		return err
+	}
+
+	return err
+}
