5b11063613afa9aff9d101539142d7c985fb3a51
siulee2 <siulee@a41ventures.com>
2022-06-19 20:02:46 +0900

    chore: add oracle bot addrerss to parameter

--- scripts/run_single_node.sh
+++ scripts/run_single_node.sh
@@ -29,6 +29,9 @@ cat $HOME/.novad/validator/config/genesis.json | jq '.app_state["crisis"]["const
 cat $HOME/.novad/validator/config/genesis.json | jq '.app_state["gov"]["voting_params"]["voting_period"]="60s"' > $HOME/.novad/validator/config/tmp_genesis.json && mv $HOME/.novad/validator/config/tmp_genesis.json $HOME/.novad/validator/config/genesis.json
 cat $HOME/.novad/validator/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="unova"' > $HOME/.novad/validator/config/tmp_genesis.json && mv $HOME/.novad/validator/config/tmp_genesis.json $HOME/.novad/validator/config/genesis.json
 
+# update oracle params
+cat $HOME/.novad/validator/config/genesis.json | jq '.app_state["oracle"]["params"]["oracle_operators"][0]="nova1lds58drg8lvnaprcue2sqgfvjnz5ljlkq9lsyf"' > $HOME/.novad/validator/config/tmp_genesis.json && mv $HOME/.novad/validator/config/tmp_genesis.json $HOME/.novad/validator/config/genesis.json
+
 # validator
 # enable rest api server & unsafe cors
 sed -i -E 's|enable = false|enable = true|g' $HOME/.novad/validator/config/app.toml
