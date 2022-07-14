3577c15c843782afc8a02b9d3a125860c78f5644
ellie <ellie@a41ventures.com>
2022-07-15 03:29:42 +0900

    feat: add commission params

--- x/ibcstaking/types/genesis.go
+++ x/ibcstaking/types/genesis.go
@@ -1,7 +1,5 @@
 package types
 
-import sdk "github.com/cosmos/cosmos-sdk/types"
-
 // NewGenesisState creates a new GenesisState object
 func NewGenesisState(params Params) *GenesisState {
 	return &GenesisState{
@@ -19,11 +17,8 @@ func DefaultGenesisState() *GenesisState {
 // ValidateGenesis validates the provided genesis state to ensure the
 // expected invariants holds.
 func ValidateGenesis(gs GenesisState) error {
-	for _, zoneRegisterAddr := range gs.Params.DaoModifiers {
-		_, err := sdk.AccAddressFromBech32(zoneRegisterAddr)
-		if err != nil {
-			return err
-		}
+	if err := gs.Params.Validate(); err != nil {
+		return err
 	}
 	return nil
 }
