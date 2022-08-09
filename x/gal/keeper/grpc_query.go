c7b030929bba0d30b4c2ae2015da18c39aeadcc4
siulee2 <siulee@a41ventures.com>
2022-08-10 07:20:15 +0900

    feat: query for total claimable assets

--- x/gal/keeper/grpc_query.go
+++ x/gal/keeper/grpc_query.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"context"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	"github.com/Carina-labs/nova/x/gal/types"
 )
@@ -25,37 +26,54 @@ func (q QueryServer) Params(c context.Context, request *types.QueryParamsRequest
 	return &types.QueryParamsResponse{Params: params}, nil
 }
 
-func (q QueryServer) ClaimableAmount(ctx context.Context, request *types.ClaimableAmountRequest) (*types.ClaimableAmountResponse, error) {
+func (q QueryServer) ClaimableAmount(goCtx context.Context, request *types.ClaimableAmountRequest) (*types.ClaimableAmountResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	zone, ok := q.keeper.ibcstakingKeeper.GetRegisteredZone(ctx, request.ZoneId)
+	if !ok {
+		return nil, sdkerrors.Wrap(types.ErrNotFoundZoneInfo, "zone not found")
+	}
+
+	addr, err := sdk.AccAddressFromBech32(request.Address)
+	if err != nil {
+		return nil, sdkerrors.Wrap(types.ErrInvalidAddress, err.Error())
+	}
+
+	assets, err := q.keeper.TotalClaimableAssets(ctx, zone, request.IcaTransferPortId, request.IcaTransferChannelId, addr)
+	if err != nil {
+		return nil, sdkerrors.Wrapf(types.ErrUnknown, "failed to get total claimable assets: %s", err.Error())
+	}
+
+	return &types.ClaimableAmountResponse{
+		Amount: *assets,
+	}, nil
+}
+
+func (q QueryServer) PendingWithdrawals(goCtx context.Context, request *types.PendingWithdrawalsRequest) (*types.PendingWithdrawalsResponse, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (q QueryServer) PendingWithdrawals(ctx context.Context, request *types.PendingWithdrawalsRequest) (*types.PendingWithdrawalsResponse, error) {
+func (q QueryServer) ActiveWithdrawals(goCtx context.Context, request *types.ActiveWithdrawalsRequest) (*types.ActiveWithdrawalsResponse, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (q QueryServer) ActiveWithdrawals(ctx context.Context, request *types.ActiveWithdrawalsRequest) (*types.ActiveWithdrawalsResponse, error) {
+func (q QueryServer) Share(goCtx context.Context, request *types.QueryMyShareRequest) (*types.QueryMyShareResponse, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (q QueryServer) Share(ctx context.Context, request *types.QueryMyShareRequest) (*types.QueryMyShareResponse, error) {
+func (q QueryServer) DepositHistory(goCtx context.Context, request *types.QueryDepositHistoryRequest) (*types.QueryDepositHistoryResponse, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (q QueryServer) DepositHistory(ctx context.Context, request *types.QueryDepositHistoryRequest) (*types.QueryDepositHistoryResponse, error) {
+func (q QueryServer) UndelegateHistory(goCtx context.Context, request *types.QueryUndelegateHistoryRequest) (*types.QueryUndelegateHistoryResponse, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (q QueryServer) UndelegateHistory(ctx context.Context, request *types.QueryUndelegateHistoryRequest) (*types.QueryUndelegateHistoryResponse, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (q QueryServer) WithdrawHistory(ctx context.Context, request *types.QueryWithdrawHistoryRequest) (*types.QueryWithdrawHistoryResponse, error) {
+func (q QueryServer) WithdrawHistory(goCtx context.Context, request *types.QueryWithdrawHistoryRequest) (*types.QueryWithdrawHistoryResponse, error) {
 	//TODO implement me
 	panic("implement me")
 }
