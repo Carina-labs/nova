c1cd2f74b8f57edba9ba5c964ae6d650ccccd1d2
siulee2 <siulee@a41ventures.com>
2022-08-09 17:41:03 +0900

    feat: querier interface definition

--- x/gal/keeper/grpc_query.go
+++ x/gal/keeper/grpc_query.go
@@ -18,29 +18,29 @@ func NewQueryServer(keeper *Keeper) *QueryServer {
 	}
 }
 
-func (g QueryServer) Params(c context.Context, request *types.QueryParamsRequest) (*types.QueryParamsResponse, error) {
+func (q QueryServer) Params(c context.Context, request *types.QueryParamsRequest) (*types.QueryParamsResponse, error) {
 	ctx := sdk.UnwrapSDKContext(c)
-	params := g.keeper.GetParams(ctx)
+	params := q.keeper.GetParams(ctx)
 
 	return &types.QueryParamsResponse{Params: params}, nil
 }
 
-func (g QueryServer) Share(ctx context.Context, request *types.QueryCacheDepositAmountRequest) (*types.QueryCachedDepositAmountResponse, error) {
+func (q QueryServer) Share(ctx context.Context, request *types.QueryCacheDepositAmountRequest) (*types.QueryCachedDepositAmountResponse, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (g QueryServer) DepositHistory(ctx context.Context, request *types.QueryDepositHistoryRequest) (*types.QueryDepositHistoryResponse, error) {
+func (q QueryServer) DepositHistory(ctx context.Context, request *types.QueryDepositHistoryRequest) (*types.QueryDepositHistoryResponse, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (g QueryServer) UndelegateHistory(ctx context.Context, request *types.QueryUndelegateHistoryRequest) (*types.QueryUndelegateHistoryResponse, error) {
+func (q QueryServer) UndelegateHistory(ctx context.Context, request *types.QueryUndelegateHistoryRequest) (*types.QueryUndelegateHistoryResponse, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (g QueryServer) WithdrawHistory(ctx context.Context, request *types.QueryWithdrawHistoryRequest) (*types.QueryWithdrawHistoryResponse, error) {
+func (q QueryServer) WithdrawHistory(ctx context.Context, request *types.QueryWithdrawHistoryRequest) (*types.QueryWithdrawHistoryResponse, error) {
 	//TODO implement me
 	panic("implement me")
 }
