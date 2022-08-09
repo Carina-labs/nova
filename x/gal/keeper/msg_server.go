329ba8361b100b9ed6c7e2c5416f6b88460f695a
ellie <ellie@a41ventures.com>
2022-08-09 22:56:08 +0900

    go lint

--- x/gal/keeper/msg_server.go
+++ x/gal/keeper/msg_server.go
@@ -340,7 +340,7 @@ func (m msgServer) ClaimSnAsset(goCtx context.Context, claimMsg *types.MsgClaimS
 	for _, record := range records.Records {
 		oracleVersion := m.keeper.oracleKeeper.GetOracleVersion(ctx, zoneInfo.ZoneId)
 
-		if uint64(record.OracleVersion) >= oracleVersion {
+		if record.OracleVersion >= oracleVersion {
 			return nil, fmt.Errorf("oracle is not updated. current oracle version: %d", oracleVersion)
 		}
 		if record.State == int64(DELEGATE_SUCCESS) {
