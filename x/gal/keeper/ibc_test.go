571abc3ca4eb641917efeae9035a913d4c12e2bc
siulee2 <siulee@a41ventures.com>
2022-08-10 09:34:31 +0900

    test: claimable asset query test codes

--- x/gal/keeper/ibc_test.go
+++ x/gal/keeper/ibc_test.go
@@ -11,9 +11,9 @@ import (
 )
 
 var (
+	transferPort       = "transfer"
 	transferChannel    = "channel-0"
 	transferConnection = "connection-0"
-	transferPort       = "transfer"
 	icaConnection      = "connection-1"
 
 	baseDenom    = "unova"
@@ -25,30 +25,13 @@ var (
 
 	oracleOperatorAcc = sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address())
 
-	hostId        = "cosmos-1"
-	hostBaseDenom = "stake"
-	hostIbcDenom  = ParseAddressToIbcAddress(transferPort, transferChannel, hostBaseDenom)
+	zoneId        = "cosmos-1"
+	zoneBaseDenom = "stake"
+	zoneIbcDeno   = ParseAddressToIbcAddress(transferPort, transferChannel, zoneBaseDenom)
 
 	undelegateMsgName  = "/cosmos.staking.v1beta1.MsgUndelegate"
 	ibcTransferMsgName = "/ibc.applications.transfer.v1.MsgTransfer"
 )
-
-func (suite *KeeperTestSuite) SetupTest() {
-	suite.Setup()
-
-	suite.coordinator = novatesting.NewCoordinator(suite.T(), 2)
-	suite.chainA = suite.coordinator.GetChain(novatesting.GetChainID(1))
-	suite.chainB = suite.coordinator.GetChain(novatesting.GetChainID(2))
-
-	suite.transferPath = newIbcTransferPath(suite.chainA, suite.chainB)
-	suite.coordinator.Setup(suite.transferPath)
-
-	suite.icaPath = newIcaPath(suite.chainA, suite.chainB)
-	suite.coordinator.SetupConnections(suite.icaPath)
-	suite.icaOwnerAddr = baseOwnerAcc
-	err := setupIcaPath(suite.icaPath, suite.icaOwnerAddr.String())
-	suite.Require().NoError(err)
-}
 
 func (suite *KeeperTestSuite) SetIbcZone(zoneMsg []ibcstakingtypes.RegisteredZone) {
 	for _, msg := range zoneMsg {
@@ -140,7 +123,7 @@ func registerInterchainAccount(e *novatesting.Endpoint, owner string) error {
 func newBaseRegisteredZone() *ibcstakingtypes.RegisteredZone {
 	icaControllerPort, _ := icatypes.NewControllerPortID(baseOwnerAcc.String())
 	return &ibcstakingtypes.RegisteredZone{
-		ZoneId: hostId,
+		ZoneId: zoneId,
 		IcaConnectionInfo: &ibcstakingtypes.IcaConnectionInfo{
 			ConnectionId: icaConnection,
 			PortId:       icaControllerPort,
@@ -150,7 +133,7 @@ func newBaseRegisteredZone() *ibcstakingtypes.RegisteredZone {
 			HostAddress:        baseHostAcc.String(),
 		},
 		ValidatorAddress: "",
-		BaseDenom:        hostBaseDenom,
+		BaseDenom:        zoneBaseDenom,
 		SnDenom:          baseSnDenom,
 	}
 }
