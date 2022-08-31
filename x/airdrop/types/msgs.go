c31cba3fefd85271ed37ea4ebc83dc57ae7a41c8
scalalang2 <cailhuiris@gmail.com>
2022-09-01 00:40:28 +0900

    feat(airdrop): write for module types & msg test codes

--- x/airdrop/types/msgs.go
+++ x/airdrop/types/msgs.go
@@ -1,37 +1,89 @@
 package types
 
-import sdk "github.com/cosmos/cosmos-sdk/types"
+import (
+	"fmt"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
 
 var _ sdk.Msg = &MsgClaimAirdropRequest{}
 var _ sdk.Msg = &MsgMarkSocialQuestPerformedRequest{}
 var _ sdk.Msg = &MsgMarkUserProvidedLiquidityRequest{}
 
 func (m *MsgClaimAirdropRequest) ValidateBasic() error {
-	//TODO implement me
-	panic("implement me")
+	_, err := sdk.AccAddressFromBech32(m.UserAddress)
+	if err != nil {
+		return err
+	}
+
+	switch m.QuestType {
+	case QuestType_QUEST_NOTHING_TO_DO:
+		return nil
+	case QuestType_QUEST_SOCIAL:
+		return nil
+	case QuestType_QUEST_PROVIDE_LIQUIDITY:
+		return nil
+	case QuestType_QUEST_SN_ASSET_CLAIM:
+		return nil
+	case QuestType_QUEST_VOTE_ON_PROPOSALS:
+		return nil
+	default:
+		return fmt.Errorf("invalid quest type: %v", m.QuestType)
+	}
 }
 
 func (m *MsgClaimAirdropRequest) GetSigners() []sdk.AccAddress {
-	//TODO implement me
-	panic("implement me")
+	userAddr, err := sdk.AccAddressFromBech32(m.UserAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	return []sdk.AccAddress{userAddr}
 }
 
 func (m *MsgMarkSocialQuestPerformedRequest) ValidateBasic() error {
-	//TODO implement me
-	panic("implement me")
+	_, err := sdk.AccAddressFromBech32(m.ControllerAddress)
+	if err != nil {
+		return err
+	}
+
+	for _, addr := range m.UserAddresses {
+		if _, err := sdk.AccAddressFromBech32(addr); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (m *MsgMarkSocialQuestPerformedRequest) GetSigners() []sdk.AccAddress {
-	//TODO implement me
-	panic("implement me")
+	controllerAddr, err := sdk.AccAddressFromBech32(m.ControllerAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	return []sdk.AccAddress{controllerAddr}
 }
 
 func (m *MsgMarkUserProvidedLiquidityRequest) ValidateBasic() error {
-	//TODO implement me
-	panic("implement me")
+	_, err := sdk.AccAddressFromBech32(m.ControllerAddress)
+	if err != nil {
+		return err
+	}
+
+	for _, addr := range m.UserAddresses {
+		if _, err := sdk.AccAddressFromBech32(addr); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (m *MsgMarkUserProvidedLiquidityRequest) GetSigners() []sdk.AccAddress {
-	//TODO implement me
-	panic("implement me")
+	controllerAddr, err := sdk.AccAddressFromBech32(m.ControllerAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	return []sdk.AccAddress{controllerAddr}
 }
