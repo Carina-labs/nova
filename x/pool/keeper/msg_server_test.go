ae0bed693d0a7b0477581ed30316a8bd93176d04
boosik@a41ventures.com <boosik@a41ventures.com>
2022-09-01 16:08:58 +0900

    tc: add failed tc for SetMultiplePoolWeight

--- x/pool/keeper/msg_server_test.go
+++ x/pool/keeper/msg_server_test.go
@@ -44,7 +44,7 @@ func (suite *KeeperTestSuite) TestCreateIncentivePool_withInvalidOperator() {
 	suite.Nil(res)
 }
 
-func (suite *KeeperTestSuite) TestSetMultiplePoolWeight() {
+func (suite *KeeperTestSuite) TestSetMultiplePoolWeight_withOperator() {
 	poolKeeper := suite.App.PoolKeeper
 	// set operator
 	poolKeeper.SetParams(suite.Ctx, types.NewParams([]string{TestOperator.String()}))
@@ -97,4 +97,33 @@ func (suite *KeeperTestSuite) TestSetMultiplePoolWeight() {
 	suite.Equal(uint64(2), pool.Weight)
 	pool, err = poolKeeper.FindIncentivePoolById(suite.Ctx, "pool-3")
 	suite.Equal(uint64(5), pool.Weight)
+}
+
+func (suite *KeeperTestSuite) TestSetMultiplePoolWeight_withInvalidOperator() {
+	poolKeeper := suite.App.PoolKeeper
+	// set operator
+	poolKeeper.SetParams(suite.Ctx, types.NewParams([]string{TestOperator.String()}))
+	msgServer := suite.msgServer
+
+	// preset
+	err := poolKeeper.CreateIncentivePool(suite.Ctx, &types.IncentivePool{
+		PoolId:              "pool-1",
+		PoolContractAddress: "pool-contract-1",
+		Weight:              uint64(1),
+	})
+	suite.NoError(err)
+
+	fakeOperator := sdk.AccAddress(secp256k1.GenPrivKey().PubKey().Address())
+	msg := &types.MsgSetMultiplePoolWeight{
+		NewPoolData: []types.NewPoolWeight{
+			{
+				PoolId:    "pool-1",
+				NewWeight: uint64(3),
+			},
+		},
+		Operator: fakeOperator.String(),
+	}
+	res, err := msgServer.SetMultiplePoolWeight(sdk.WrapSDKContext(suite.Ctx), msg)
+	suite.Error(err)
+	suite.Nil(res)
 }
