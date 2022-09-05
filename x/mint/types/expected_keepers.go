8e848e754377cf5f42b862cd8fe525eaec83d22c
boosik@a41ventures.com <boosik@a41ventures.com>
2022-09-05 20:08:28 +0900

    fix: delete unused interface of expected keeper in mint module

--- x/mint/types/expected_keepers.go
+++ x/mint/types/expected_keepers.go
@@ -23,7 +23,6 @@ type AccountKeeper interface {
 // BankKeeper defines the contract needed to be fulfilled for banking and supply
 // dependencies.
 type BankKeeper interface {
-	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 	SendCoinsFromModuleToModule(ctx sdk.Context, senderModule, recipientModule string, amt sdk.Coins) error
 	MintCoins(ctx sdk.Context, name string, amt sdk.Coins) error
 	GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin
