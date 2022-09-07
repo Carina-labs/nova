f76b793c8e99068474e291ef099a62ae5cabe8a4
boosik@a41ventures.com <boosik@a41ventures.com>
2022-09-07 11:01:08 +0900

    wip: poolincentive gov

--- x/poolincentive/types/params.pb.go
+++ x/poolincentive/types/params.pb.go
@@ -66,8 +66,61 @@ func (m *Params) GetOperators() []string {
 	return nil
 }
 
+type DistributionRecord struct {
+	PoolId string `protobuf:"bytes,1,opt,name=pool_id,json=poolId,proto3" json:"pool_id,omitempty"`
+	Weight uint64 `protobuf:"varint,2,opt,name=weight,proto3" json:"weight,omitempty"`
+}
+
+func (m *DistributionRecord) Reset()         { *m = DistributionRecord{} }
+func (m *DistributionRecord) String() string { return proto.CompactTextString(m) }
+func (*DistributionRecord) ProtoMessage()    {}
+func (*DistributionRecord) Descriptor() ([]byte, []int) {
+	return fileDescriptor_47b262ef8df3512d, []int{1}
+}
+func (m *DistributionRecord) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *DistributionRecord) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_DistributionRecord.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *DistributionRecord) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_DistributionRecord.Merge(m, src)
+}
+func (m *DistributionRecord) XXX_Size() int {
+	return m.Size()
+}
+func (m *DistributionRecord) XXX_DiscardUnknown() {
+	xxx_messageInfo_DistributionRecord.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_DistributionRecord proto.InternalMessageInfo
+
+func (m *DistributionRecord) GetPoolId() string {
+	if m != nil {
+		return m.PoolId
+	}
+	return ""
+}
+
+func (m *DistributionRecord) GetWeight() uint64 {
+	if m != nil {
+		return m.Weight
+	}
+	return 0
+}
+
 func init() {
-	proto.RegisterType((*Params)(nil), "nova.pool_incentive.v1.Params")
+	proto.RegisterType((*Params)(nil), "nova.poolincentive.v1.Params")
+	proto.RegisterType((*DistributionRecord)(nil), "nova.poolincentive.v1.DistributionRecord")
 }
 
 func init() {
@@ -75,19 +128,23 @@ func init() {
 }
 
 var fileDescriptor_47b262ef8df3512d = []byte{
-	// 189 bytes of a gzipped FileDescriptorProto
+	// 248 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x52, 0xca, 0xcb, 0x2f, 0x4b,
 	0xd4, 0x2f, 0xc8, 0xcf, 0xcf, 0xc9, 0xcc, 0x4b, 0x4e, 0xcd, 0x2b, 0xc9, 0x2c, 0x4b, 0xd5, 0x2f,
 	0x33, 0xd4, 0x2f, 0x48, 0x2c, 0x4a, 0xcc, 0x2d, 0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x12,
-	0x03, 0xa9, 0xd1, 0x03, 0xa9, 0x89, 0x87, 0x2b, 0xd2, 0x2b, 0x33, 0x94, 0x12, 0x49, 0xcf, 0x4f,
-	0xcf, 0x07, 0x2b, 0xd1, 0x07, 0xb1, 0x20, 0xaa, 0x95, 0x74, 0xb8, 0xd8, 0x02, 0xc0, 0xba, 0x85,
-	0x64, 0xb8, 0x38, 0xf3, 0x0b, 0x52, 0x8b, 0x12, 0x4b, 0xf2, 0x8b, 0x8a, 0x25, 0x18, 0x15, 0x98,
-	0x35, 0x38, 0x83, 0x10, 0x02, 0x56, 0x2c, 0x33, 0x16, 0xc8, 0x33, 0x38, 0x79, 0x9f, 0x78, 0x24,
-	0xc7, 0x78, 0xe1, 0x91, 0x1c, 0xe3, 0x83, 0x47, 0x72, 0x8c, 0x13, 0x1e, 0xcb, 0x31, 0x5c, 0x78,
-	0x2c, 0xc7, 0x70, 0xe3, 0xb1, 0x1c, 0x43, 0x94, 0x61, 0x7a, 0x66, 0x49, 0x46, 0x69, 0x92, 0x5e,
-	0x72, 0x7e, 0xae, 0xbe, 0x73, 0x62, 0x51, 0x66, 0x5e, 0xa2, 0x6e, 0x4e, 0x62, 0x52, 0xb1, 0x3e,
-	0xd8, 0xc1, 0x15, 0x68, 0x4e, 0x2e, 0xa9, 0x2c, 0x48, 0x2d, 0x4e, 0x62, 0x03, 0xbb, 0xc0, 0x18,
-	0x10, 0x00, 0x00, 0xff, 0xff, 0x0c, 0x0b, 0x6c, 0x07, 0xd5, 0x00, 0x00, 0x00,
+	0x05, 0xa9, 0xd1, 0x43, 0x51, 0xa3, 0x57, 0x66, 0x28, 0x25, 0x92, 0x9e, 0x9f, 0x9e, 0x0f, 0x56,
+	0xa1, 0x0f, 0x62, 0x41, 0x14, 0x2b, 0xe9, 0x70, 0xb1, 0x05, 0x80, 0x35, 0x0b, 0xc9, 0x70, 0x71,
+	0xe6, 0x17, 0xa4, 0x16, 0x25, 0x96, 0xe4, 0x17, 0x15, 0x4b, 0x30, 0x2a, 0x30, 0x6b, 0x70, 0x06,
+	0x21, 0x04, 0xac, 0x58, 0x66, 0x2c, 0x90, 0x67, 0x50, 0x72, 0xe5, 0x12, 0x72, 0xc9, 0x2c, 0x2e,
+	0x29, 0xca, 0x4c, 0x2a, 0x2d, 0xc9, 0xcc, 0xcf, 0x0b, 0x4a, 0x4d, 0xce, 0x2f, 0x4a, 0x11, 0x12,
+	0xe7, 0x62, 0x07, 0xd9, 0x16, 0x9f, 0x99, 0x22, 0xc1, 0xa8, 0xc0, 0xa8, 0xc1, 0x19, 0xc4, 0x06,
+	0xe2, 0x7a, 0xa6, 0x08, 0x89, 0x71, 0xb1, 0x95, 0xa7, 0x66, 0xa6, 0x67, 0x94, 0x48, 0x30, 0x29,
+	0x30, 0x6a, 0xb0, 0x04, 0x41, 0x79, 0x4e, 0xde, 0x27, 0x1e, 0xc9, 0x31, 0x5e, 0x78, 0x24, 0xc7,
+	0xf8, 0xe0, 0x91, 0x1c, 0xe3, 0x84, 0xc7, 0x72, 0x0c, 0x17, 0x1e, 0xcb, 0x31, 0xdc, 0x78, 0x2c,
+	0xc7, 0x10, 0x65, 0x98, 0x9e, 0x59, 0x92, 0x51, 0x9a, 0xa4, 0x97, 0x9c, 0x9f, 0xab, 0xef, 0x9c,
+	0x58, 0x94, 0x99, 0x97, 0xa8, 0x9b, 0x93, 0x98, 0x54, 0xac, 0x0f, 0xf6, 0x76, 0x05, 0x9a, 0xc7,
+	0x4b, 0x2a, 0x0b, 0x52, 0x8b, 0x93, 0xd8, 0xc0, 0x1e, 0x31, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff,
+	0x64, 0x15, 0x26, 0x92, 0x1b, 0x01, 0x00, 0x00,
 }
 
 func (m *Params) Marshal() (dAtA []byte, err error) {
@@ -122,6 +179,41 @@ func (m *Params) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
+func (m *DistributionRecord) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *DistributionRecord) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *DistributionRecord) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.Weight != 0 {
+		i = encodeVarintParams(dAtA, i, uint64(m.Weight))
+		i--
+		dAtA[i] = 0x10
+	}
+	if len(m.PoolId) > 0 {
+		i -= len(m.PoolId)
+		copy(dAtA[i:], m.PoolId)
+		i = encodeVarintParams(dAtA, i, uint64(len(m.PoolId)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
 func encodeVarintParams(dAtA []byte, offset int, v uint64) int {
 	offset -= sovParams(v)
 	base := offset
@@ -144,6 +236,22 @@ func (m *Params) Size() (n int) {
 			l = len(s)
 			n += 1 + l + sovParams(uint64(l))
 		}
+	}
+	return n
+}
+
+func (m *DistributionRecord) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.PoolId)
+	if l > 0 {
+		n += 1 + l + sovParams(uint64(l))
+	}
+	if m.Weight != 0 {
+		n += 1 + sovParams(uint64(m.Weight))
 	}
 	return n
 }
@@ -215,6 +323,107 @@ func (m *Params) Unmarshal(dAtA []byte) error {
 			}
 			m.Operators = append(m.Operators, string(dAtA[iNdEx:postIndex]))
 			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipParams(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthParams
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *DistributionRecord) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowParams
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: DistributionRecord: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: DistributionRecord: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field PoolId", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowParams
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthParams
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthParams
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.PoolId = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 2:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Weight", wireType)
+			}
+			m.Weight = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowParams
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.Weight |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
 		default:
 			iNdEx = preIndex
 			skippy, err := skipParams(dAtA[iNdEx:])
