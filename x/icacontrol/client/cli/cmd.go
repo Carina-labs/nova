0fe4d81ede6897a4d524a68d83d2a1349ccefb8c
ellie@a41ventures.com <ellie@a41ventures.com>
2022-09-21 18:49:25 +0900

    add icacontrol cmd.go file

--- x/icacontrol/client/cli/cmd.go
+++ x/icacontrol/client/cli/cmd.go
@@ -1,1 +1,51 @@
 package cli
+
+import (
+	"fmt"
+	"github.com/Carina-labs/nova/x/icacontrol/types"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/spf13/cobra"
+)
+
+// GetTxCmd creates and returns the icacontrol tx command
+func GetTxCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("%s transactions subcommands", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	cmd.AddCommand(
+		txRegisterZoneCmd(),
+		txDelegateTxCmd(),
+		txUndelegateTxCmd(),
+		txAutoStakingTxCmd(),
+		txTransferTxCmd(),
+		txDeleteZoneTxCmd(),
+		txChangeZoneInfoTxCmd(),
+		txAuthzGrantTxCmd(),
+		txAuthzRevokeTxCmd(),
+	)
+
+	return cmd
+}
+
+// GetQueryCmd creates and returns the icacontrol query command
+func GetQueryCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      "Querying commands for the icacontrol module",
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	cmd.AddCommand(
+		queryAllZones(),
+		queryZone(),
+	)
+
+	return cmd
+}
