d7e66e5a34f0270b54315e46f9c1e9dc28ed270b
ellie@a41ventures.com <ellie@a41ventures.com>
2022-09-21 18:18:10 +0900

    test: add ica control query

--- x/icacontrol/client/cli/query.go
+++ x/icacontrol/client/cli/query.go
@@ -6,14 +6,53 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// GetQueryCmd creates and returns the icacontrol query command
-func GetQueryCmd() *cobra.Command {
+func queryAllZones() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:                        types.ModuleName,
-		Short:                      "Querying commands for the icacontrol module",
-		DisableFlagParsing:         true,
-		SuggestionsMinimumDistance: 2,
-		RunE:                       client.ValidateCmd,
+		Use:  "all-zone",
+		Long: "Query for all zone",
+		Args: cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.AllZones(cmd.Context(), &types.QueryAllZonesRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	return cmd
+}
+
+func queryZone() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:  "zone",
+		Long: "Query for zone",
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.Zone(cmd.Context(), &types.QueryZoneRequest{
+				ZoneId:  args[0],
+			})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
 	}
 
 	return cmd
