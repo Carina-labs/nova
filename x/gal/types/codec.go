709c77beb6e34796709b89688ad37f1f8832f577
ellie@a41ventures.com <ellie@a41ventures.com>
2022-11-03 15:11:18 +0900

    chore: add gal module codec

--- x/gal/types/codec.go
+++ x/gal/types/codec.go
@@ -13,11 +13,25 @@ var (
 )
 
 func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
+	cdc.RegisterConcrete(MsgDeposit{}, "gal/MsgDeposit", nil)
+	cdc.RegisterConcrete(MsgDelegate{}, "gal/MsgDelegate", nil)
+	cdc.RegisterConcrete(MsgClaimSnAsset{}, "gal/MsgClaimSnAsset", nil)
+	cdc.RegisterConcrete(MsgWithdraw{}, "gal/MsgWithdraw", nil)
+	cdc.RegisterConcrete(MsgUndelegate{}, "gal/MsgUndelegate", nil)
+	cdc.RegisterConcrete(MsgPendingUndelegate{}, "gal/MsgPendingUndelegate", nil)
+	cdc.RegisterConcrete(MsgIcaWithdraw{}, "gal/MsgIcaWithdraw", nil)
 }
 
 func RegisterInterfaces(registry types.InterfaceRegistry) {
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 	registry.RegisterImplementations(
 		(*sdk.Msg)(nil),
+		&MsgDeposit{},
+		&MsgDelegate{},
+		&MsgClaimSnAsset{},
+		&MsgWithdraw{},
+		&MsgUndelegate{},
+		&MsgPendingUndelegate{},
+		&MsgIcaWithdraw{},
 	)
 }
