9205e629642d13bbb5b0fd6a21288f66d44d03a9
ellie@a41ventures.com <ellie@a41ventures.com>
2022-11-24 17:31:15 +0900

    refactor: modify gal module version

--- x/gal/module.go
+++ x/gal/module.go
@@ -147,7 +147,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 
 // ConsensusVersion implements AppModule/ConsensusVersion.
 func (AppModule) ConsensusVersion() uint64 {
-	return 3
+	return 1
 }
 
 // BeginBlock performs a no-op.
