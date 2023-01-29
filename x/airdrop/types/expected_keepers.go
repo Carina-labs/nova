fc63d8af31f74b4170e76a4fba38d1a3e344fec5
ellie@a41ventures.com <ellie@a41ventures.com>
2023-01-29 19:11:34 +0900

    go fmt

--- x/airdrop/types/expected_keepers.go
+++ x/airdrop/types/expected_keepers.go
@@ -14,4 +14,3 @@ type BankKeeper interface {
 	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 	MintCoins(ctx sdk.Context, name string, amt sdk.Coins) error
 }
-
