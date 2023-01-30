926a97168a23203b5220d9361d647b8c646f86ef
ellie@a41ventures.com <ellie@a41ventures.com>
2023-01-30 13:51:14 +0900

    feat: airdrop token burn logic

--- x/airdrop/types/expected_keepers.go
+++ x/airdrop/types/expected_keepers.go
@@ -13,4 +13,6 @@ type AccountKeeper interface {
 type BankKeeper interface {
 	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 	MintCoins(ctx sdk.Context, name string, amt sdk.Coins) error
+	BurnCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
+	GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin
 }
